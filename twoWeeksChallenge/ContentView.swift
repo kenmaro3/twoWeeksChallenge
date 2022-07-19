4c2c1742273faf47c975bb52e965a7ae67e9025e
kenmaro3 <miha.ken.19@gmail.com>
2022-07-19 12:25:15 +0900

    Initial Commit

---
+++ twoWeeksChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twoWeeksChallenge
+//
+//  Created by Kentaro Mihara on 2022/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twoWeeksChallenge/twoWeeksChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  twoWeeksChallengeApp.swift
+//  twoWeeksChallenge
+//
+//  Created by Kentaro Mihara on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct twoWeeksChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
